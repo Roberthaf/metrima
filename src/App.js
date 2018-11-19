bb1a1772481c95df10978ffa1319f271781de39b
Róbert Hafþórsson <roberthafth@gmail.com>
2018-11-19 16:45:08 +0000

    add new graphql schema

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Amplify, { Analytics, Auth, Storage, API, graphqlOperation } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import aws_exports from './aws-exports';
 import './App.css';
+import { getUser } from './graphql/queries';
+import { id } from 'postcss-selector-parser';
+
+Amplify.configure(aws_exports);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: {}, 
+      user: "",
+      company: "",
+      role: "",
+    }
+    
+  }
+
+  componentDidMount() {
+    Analytics.record('Amplify_CLI');
+    
+    Auth.currentAuthenticatedUser()
+    .then(user => {
+      const items = {
+        id: user.attributes.sub
+      }
+      const getUserInfo =  API.graphql(graphqlOperation(getUser,items));  
+      console.log(getUserInfo)
+    })
+    .catch(err => 
+      console.log(err));
+  }
+
+  getUserData = async (items) => {
+    const getUserInfo =  await API.graphql(graphqlOperation(getUser,items));
+    //getUserData
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h3>Counting made easy!</h3>
+
       </div>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App, true);
